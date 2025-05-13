ffec8e0ab0c2cc346633a67bde5537de28e3e957
freedsolo24 <freedsolo24@gmail.com>
2025-05-13 16:46:57 +0800

    2

--- shellscript.sh
+++ shellscript.sh
@@ -1,9 +1,25 @@
 #!"C:\Program Files\Git\bin\bash.exe"
 
+function readLine {
 declare -i LINE=0
-
-while read p
-do
+while read p;do
   LINE=$(($LINE+1))
 done < 'c:\Users\WXR\projects\linux_shell\newcoder.txt'
 echo "本文件的行数=$LINE"
+}
+
+function last5Line {
+  declare -i ROW=0
+  while read p;do
+    ROW=$((${ROW}+1))
+  done < 'c:\Users\WXR\projects\linux_shell\newcoder.txt'
+  while read LINE ;do
+    if [ ${ROW} -le 5 -a ${ROW} -ge 1 ];then
+      echo ${LINE}
+    fi
+    ROW=$((${ROW}-1))
+  done < 'c:\Users\WXR\projects\linux_shell\newcoder.txt'
+}
+
+readLine
+last5Line
