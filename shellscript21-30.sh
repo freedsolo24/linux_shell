ebfff8156aa2b600376c2e886859186d801649b3
freesouru <freedsolo24@gmail.com>
2025-07-09 10:42:49 +0800

    7/9/2025

--- shellscript21-30.sh
+++ shellscript21-30.sh
@@ -15,8 +15,33 @@ while read -r line || [[ -n ${line} ]];do
     echo "${line}"
         
 done < ./newcoder_21.txt
+}
+
+function shell22() {
+declare -A map
+
+# 以冒号为分隔符, 放到map里. 多个相同的key, value以空格分隔
+while IFS=':' read -r -a line || [[ -n ${line[*]} ]];do
+# 将值存入字典 "111": "13443 13643"
+    map[${line[0]}]="${map[${line[0]}]} ${line[1]} "
+done < ./newcoder_22.txt
+
+# 将字典 key 排序后输出
+for key in $(echo "${!map[@]}" | sort -n);do
+    echo "[${key}]"
+    for value in ${map[${key}]};do
+        echo "${value}"
+    done
+done
+
+
+
+
+
 
 }
 
 echo -e "\033[31;42mshell21 格式化输出\033[0m"
 shell21
+echo -e "\033[31;42mshell22 处理文本\033[0m"
+shell22
